48a1271c0bfe20eecb32600895abc59e9426ff9e
Wilson Lopez Franco <wilopuan@hotmail.com>
2021-05-07 14:25:17 -0500

    Inicializacion desarrollo en repo local

---
+++ src/main/java/com/wlopez/ianamanagement/IanaManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wlopez.ianamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IanaManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IanaManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wlopez/ianamanagement/IanaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlopez.ianamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IanaManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
